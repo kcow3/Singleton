ff9af4fae2cc286769bd72c13ad1d16f996aa5ad
Kevin Cowdrey <cowdrey.kevin@outlook.com>
2020-01-28 21:53:14 +0200

    Push init commit

---
+++ SingletonPattern1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonPattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton!");
+            Singleton.GetInstance.SetCounter(211);
+            var c1 = Singleton.GetInstance.GetCounter();
+            Singleton.GetInstance.Counter2 = 3;
+            var c2 = Singleton.GetInstance.Counter2;
+        }
+    }
+}
---
+++ SingletonPattern1/Singleton.cs
@@ -0,0 +1,45 @@
+﻿namespace SingletonPattern1
+{
+    public sealed class Singleton
+    {
+        //private, parameterless constructor  
+        private Singleton() { }
+
+        private int _counter = 0;
+
+
+        //static var that holds ref of the object created
+        public static Singleton instance = null;
+
+        //object to make thread safe
+        public static readonly object _lock = new object();
+
+        //method to get reference to object
+        public static Singleton GetInstance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void SetCounter(int x)
+        {
+            _counter = x;
+        }
+
+        public int GetCounter()
+        {
+            return _counter;
+        }
+
+        public int Counter2 { get; set; }
+    }
+}
